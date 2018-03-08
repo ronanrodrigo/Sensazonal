ad1d67e028f1e7623823c18b9c0c8fdcb5e52234
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2018-03-08 10:18:07 -0300

    Create NSPersistentContainer extension + Log entity + DataModel

---
+++ Sensazonal/Essentials/NSPersistentContainerExtension.swift
@@ -0,0 +1,22 @@
+import Foundation
+import CoreData
+
+extension NSPersistentContainer {
+
+    static let singleton: NSPersistentContainer = {
+        let dataModelFileName = "Sensazonal"
+        let container = NSPersistentContainer(name: dataModelFileName)
+        container.loadPersistentStores { Log.coreDataError($1) }
+        return container
+    }()
+
+    static func saveContext() {
+        guard singleton.viewContext.hasChanges else { return }
+        do {
+            try singleton.viewContext.save()
+        } catch {
+            Log.coreDataError(error)
+        }
+    }
+
+}
