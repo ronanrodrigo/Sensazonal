387fee1c1d758213d998feaba0202b37863d7424
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2018-03-07 21:29:08 -0300

    Remove comment in LocalizedString

--- Sensazonal/Locale/Locale.swift
+++ Sensazonal/Locale/Locale.swift
@@ -4,6 +4,6 @@ struct Locale {
     private static let fileName = "Locale"
 
     static func localize(_ key: String) -> String {
-        return NSLocalizedString(key, tableName: fileName, comment: "Shu 2")
+        return NSLocalizedString(key, tableName: fileName, comment: "")
     }
 }
