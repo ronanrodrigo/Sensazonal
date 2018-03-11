88844d1db7fae98e708b401b870340848e8abfdb
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2018-03-11 08:55:55 -0300

    Fix typo

--- SensazonalUnitTests/ListFoodByMonth/FoodViewCellTests.swift
+++ SensazonalUnitTests/ListFoodByMonth/FoodViewCellTests.swift
@@ -8,7 +8,7 @@ final class FoodViewCellTests: FBSnapshotTestCase {
 
     func testViewCreationWhenBindedWithViewModelThenPresentFoodsForSelectedMonth() {
         let view = FoodViewCell(frame: CGRect(x: 0, y: 0, width: 100, height: 120))
-        let viewModel = FoodViewModel(name: "Morango u", nameColor: .white, nameBackgroundColor: .red,
+        let viewModel = FoodViewModel(name: "Morango", nameColor: .white, nameBackgroundColor: .red,
                                       photo: #imageLiteral(resourceName: "Strawberry"))
 
         view.bind(viewModel: viewModel)
