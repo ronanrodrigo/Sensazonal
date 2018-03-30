0115c2b96a61eccda8f8025283b511f6b026ba96
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2018-03-30 11:09:43 -0300

    Create screenshot tests to view controller

---
+++ SensazonalUnitTests/Common/ViewControllers/NavigationViewControllerTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+
+final class NavigationViewControllerTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    func testMethodNameWhenSomethingThenDoAnotherThing() {
+    }
+
+}
