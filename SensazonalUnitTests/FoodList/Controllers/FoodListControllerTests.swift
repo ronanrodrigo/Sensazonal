6f74aca3513de9628b5bbd85043e8ce818d9fec9
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2018-05-05 16:57:21 -0300

    Add open moth selector tests

--- SensazonalUnitTests/FoodList/Controllers/FoodListControllerTests.swift
+++ SensazonalUnitTests/FoodList/Controllers/FoodListControllerTests.swift
@@ -13,11 +13,23 @@ final class FoodListControllerTests: XCTestCase {
     }
 
     func testInitWhenConstructedThenExecuteFoodListInteractor() {
-        let interactor = StubListFoodByMonthInteractor(gateway: ListFoodStubGateway(), presenter: ListFoodStubPresenter())
+        let interactor = StubListFoodByMonthInteractor(gateway: ListFoodStubGateway(),
+                                                       presenter: ListFoodStubPresenter())
 
         _ = FoodListController(interactor: interactor, listViewController: UIViewController())
 
         XCTAssertTrue(interactor.didCallList)
+    }
+
+    func testOpenMonthSelectorThenPresentMonthSelectorViewController() {
+        let interactor = ListFoodByMonthInteractor(gateway: ListFoodStubGateway(), presenter: ListFoodStubPresenter())
+        let controller = FoodListController(interactor: interactor,
+                                            listViewController: FoodListViewController(nibName: nil, bundle: nil))
+        UIApplication.shared.keyWindow?.rootViewController = controller.viewController
+
+        controller.openMonthSelector(at: GregorianMonth())
+
+        XCTAssertTrue(controller.viewController.presentedViewController!.isKind(of: SelectMonthViewController.self))
     }
 
 }
