1a603e6bc5ac974239d2675a31bbe9aeb85bb18f
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2018-05-06 08:56:55 -0300

    Update FoodViewModelTests

--- SensazonalUnitTests/FoodList/ViewModels/FoodViewModelTests.swift
+++ SensazonalUnitTests/FoodList/ViewModels/FoodViewModelTests.swift
@@ -4,14 +4,12 @@ import XCTest
 final class FoodViewModelTests: XCTestCase {
 
     func testInit() {
-        let photo = UIImage()
-
         let viewModel = FoodViewModel.sample
 
         XCTAssertEqual(viewModel.name, "Name")
         XCTAssertEqual(viewModel.nameBackgroundGradient.colors?.count, 2)
-        XCTAssertEqual(viewModel.nameColor, .black)
-        XCTAssertEqual(viewModel.photo, photo)
+        XCTAssertEqual(viewModel.nameColor, .white)
+        XCTAssertEqual(viewModel.photo, #imageLiteral(resourceName: "content/strawberry"))
     }
 
 }
