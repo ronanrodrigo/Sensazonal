50e572b551dfe533b9fddc1aaf5245b27328f269
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2018-05-31 13:30:04 -0300

    Create codable entities folder

--- Sensazonal/Common/Entities/Food.swift
+++ Sensazonal/Common/Entities/Food.swift
@@ -7,17 +7,3 @@ struct Food {
     let months: [Int]
 
 }
-
-struct FoodDecodable: Decodable {
-
-    let keyName: String
-    let keyGroup: String
-    let months: [Int]
-
-    enum CodingKeys: String, CodingKey {
-        case keyName = "key_name"
-        case keyGroup = "key_group"
-        case months
-    }
-
-}
